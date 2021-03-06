9313daf5a231e8358ad8524c75099915505fd8d8
Rasmus Ploug <rasmusploug@live.dk>
2021-03-06 20:33:45 +0100

    Fixed Emils stuff

--- Assets/Scripts/Interactions/Dialog/Tree_Dialog.cs
+++ Assets/Scripts/Interactions/Dialog/Tree_Dialog.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tree_Dialog : Dialog
-{
+public class Tree_Dialog : Dialog {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Awake() {
         initStart();
-        newDialogLine("I have always wanted to climb to the top!"); moodSprite(0); //0=normal 1=sad 2=angry
+        newDialogLine("I wish i could climb up there..."); moodSprite(0); //0=normal 1=sad 2=angry
+        newDialogLine("But I'm afaid of heights"); moodSprite(1); //0=normal 1=sad 2=angry
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         dialog();
     }
+
+
 }
