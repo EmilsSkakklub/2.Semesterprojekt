35838fbaee076a36443f54104cf0ce46fe8b67b9
Limeremir <emrim19@student.sdu.dk>
2021-03-08 14:17:03 +0100

    Tree Enemy

--- Assets/Scripts/Enemies/TreeSpirit.cs
+++ Assets/Scripts/Enemies/TreeSpirit.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrototypeEnemy : Enemy
+public class TreeSpirit : Enemy
 {
     // Start is called before the first frame update
     void Start()
     {
-        initStart("Prototype Enemy", 10);
+        initStart("TreeSpirit", 20);
     }
 
     // Update is called once per frame
