c7045c78fa786b4cfdda623583d8576eb904a43e
Limeremir <emrim19@student.sdu.dk>
2021-03-09 08:47:37 +0100

    I'll have nightmares tonight reee

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -7,16 +7,71 @@ public abstract class Enemy : MonoBehaviour
     public int health;
     public string enemyName;
 
+    public Animator animator;
+    public Transform playerTransform;
+    public CharacterController controller;
+
+    public Vector3 velocity;
+    public float gravity = -9.81f;
+    public bool isGrounded;
+
+    public float moveSpeed = 1f;
+    public float MaxDistance = 1f;
+    public float MinDistance = 1f;
+
+    //indicate what animation is playing
+    public bool walkAnimation;
+
+    //hash codes for the animations
+    public int isWalkingHash;
+
     protected void initStart(string enemyName, int health) {
         gameObject.layer = LayerMask.NameToLayer("Enemy");
         setEnemyName(enemyName);
         setHealth(health);
+
+        animator = GetComponent<Animator>();
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+        controller = GetComponent<CharacterController>();
+
+        setAnimationHashCodes();
     }
 
     protected void initUpdate() {
+        getCurrentAnimationPlaying();
+        updateGravity();
+        groundCheck();
+        goTowardsEnemy();
         die();
     }
 
+    
+    private void setAnimationHashCodes() {
+        isWalkingHash = Animator.StringToHash("isWalking");
+    }
+
+    private void getCurrentAnimationPlaying() {
+        walkAnimation = animator.GetCurrentAnimatorStateInfo(0).IsTag("Walk");
+    }
+
+    private void updateGravity() {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+    }
+
+    void groundCheck() {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.07f + 0.02f)) {
+            isGrounded = true;
+        }
+        else {
+            isGrounded = false;
+        }
+    }
 
     public void takeDamage(int damage) {
        health -= damage;
@@ -28,6 +83,22 @@ public abstract class Enemy : MonoBehaviour
         }
     }
 
+    public void goTowardsEnemy() {
+        
+        transform.LookAt(playerTransform);
+
+        if (Vector3.Distance(transform.position, playerTransform.position) >= MinDistance) {
+            animator.SetBool(isWalkingHash, true);
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+            if (Vector3.Distance(transform.position, playerTransform.position) <= MaxDistance) {
+                //implement Attack
+            }
+        }
+        else {
+            animator.SetBool(isWalkingHash, false);
+        }
+    }
 
 
 
