ff2e910cf134fdf1d27cc959d819c89f87b86481
Limeremir <emrim19@student.sdu.dk>
2021-04-22 00:30:06 +0200

    Gnome Enemy UwU

--- Assets/Scripts/Enemies/SmallTreeSpirit.cs
+++ Assets/Scripts/Enemies/SmallTreeSpirit.cs
@@ -7,7 +7,7 @@ public class SmallTreeSpirit : Enemy
     // Start is called before the first frame update
     void Start()
     {
-        initStart("SmallTreeSpirit", 1, 5, 3);
+        initStart("SmallTreeSpirit", 1, 5, 3, 0.6f);
     }
 
     // Update is called once per frame
