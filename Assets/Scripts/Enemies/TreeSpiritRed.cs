521a71a790e2a1b2ba33a1d75401cd99c12af292
Limeremir <emrim19@student.sdu.dk>
2021-05-03 14:46:15 +0200

    New Tree Enemy

---
+++ Assets/Scripts/Enemies/TreeSpiritRed.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpiritRed : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        initStart("Red Tree Spirit",1, 30, 2.5f, 0.8f, 6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        initUpdate();
+    }
+}
