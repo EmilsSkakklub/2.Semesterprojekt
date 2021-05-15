43907d05f0b156ecc400f8156b9bef05128f83bf
Rasmus Ploug <rasmusploug@live.dk>
2021-05-15 17:31:31 +0200

    Smol fixes

--- Assets/Scripts/Interactions/BoxDialog/BoxL3N1.cs
+++ Assets/Scripts/Interactions/BoxDialog/BoxL3N1.cs
@@ -11,8 +11,9 @@ public class BoxL3N1 : BoxDialog
         newDialogLine("This place really creeps me out.", 1);
         newDialogLine("He must be around here somewhere!", 3);
         newDialogLine("Arthur, I sense that this place is filled with some of our old toys.", 3);
-        newDialogLine("We should find them all, this might help us find him.", 3);
-        newDialogLine("Hmm, you're probably right Teddy.", 0);
+        newDialogLine("We should find them, as they might help us!", 3);
+        newDialogLine("But we need to hurry up!", 0);
+        newDialogLine("That is true Arthur. It's your choice.", 3);
     }
 
     // Update is called once per frame
